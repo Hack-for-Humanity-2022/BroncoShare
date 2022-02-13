d69785d7151469f706a5ccca077580f088ecae77
addsch <addison148@gmail.com>
2022-02-12 23:25:35 -0800

    aaaaaaaaaa

--- submitoffer.js
+++ submitoffer.js
@@ -2,15 +2,14 @@ const Database = require("better-sqlite3");
 const createTable = "CREATE TABLE IF NOT EXISTS offers('name' TEXT, 'points' INTEGER, 'contact' TEXT)";
 
 const addOffer = (name, points, contact) => {
-    const offers = './offers.sqlite3';
+    const offers = './offers-data.sqlite3';
     let db = new Database(offers, Database.OPEN_READWRITE, (err) => {
         if (err) {
             console.error(err.message);
         }
-        verbose: console.log
+        verbose: console.log;
     });
     db.exec(createTable);
 
+    db.exec(`INSERT INTO offers(name, points, contact) VALUES('${name}', ${points}, '${contact}')`);
 }
-
-addOffer("test", 1, "test2");
